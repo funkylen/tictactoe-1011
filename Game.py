c51798d86296c74295aa5d6dc25c0c13d43e637d
xsnk1 <114852238+xsnk1@users.noreply.github.com>
2022-10-16 12:01:56 +0300

    Add files via upload

---
+++ Game.py
@@ -0,0 +1,80 @@
+import prompt
+SIGN_X = 'X'
+SIGN_O = 'O'
+
+state = [
+    [None, None, None],
+    [None, None, None],
+    [None, None, None],
+]
+
+
+def check_index(index):
+    min_value = 0
+    max_value = 2
+    return min_value <= index <= max_value
+
+
+def start():
+    steps = 9
+    while steps != -1:
+        current_sign = SIGN_O if steps % 2 == 0 else SIGN_X
+        print(f'Ходит {current_sign}')
+
+        row_index = prompt.integer('Введите строку от 1 до 3: ') - 1            
+        col_index = prompt.integer('Введите столбец от 1 до 3: ') - 1
+        if check_index(row_index) and check_index(col_index):
+
+            if state[row_index][col_index] is not None:
+                print('Выберите другое место. Занято.')
+                continue
+
+            state[row_index][col_index] = current_sign
+
+            for line in state:
+                print(line)
+
+            steps -= 1
+                
+
+
+        else:
+            print('Неправильное значение столбца или строки')
+
+        if check_win(state) == True:
+            return
+
+        if steps == 0:
+            print('Ничья')
+            return
+
+        
+
+def check_win(state):
+    if (state[0][0] == state [0][1] == state[0][2]) and state[0][2] != None:
+        print ('Победил игрок ' + str(state[0][0]))
+        return True
+    if (state[0][0] == state [1][0] == state[2][0]) and state[2][0] != None:
+        print ('Победил игрок ' + str(state[0][0]))
+        return True
+    if (state[1][0] == state [1][1] == state[1][2]) and state[1][2] != None:
+        print ('Победил игрок ' + str(state[1][0]))
+        return True
+    if (state[2][0] == state [2][1] == state[2][2]) and state[2][2] != None:
+        print ('Победил игрок ' + str(state[2][0]))
+        return True
+    if (state[0][1] == state [1][1] == state[2][1]) and state[0][1] != None:
+        print ('Победил игрок ' + str(state[0][1]))
+        return True
+    if (state[0][2] == state [1][2] == state[2][2]) and state[0][2] != None:
+        print ('Победил игрок ' + str(state[0][2]))
+        return True
+    if (state[0][0] == state [1][1] == state[2][2]) and state[0][0] != None:
+        print ('Победил игрок ' + str(state[0][2]))
+        return True
+    if (state[0][2] == state [1][1] == state[2][0]) and state[0][2] != None:
+        print ('Победил игрок ' + str(state[0][2]))
+        return True
+    else:
+        return False
+
---
+++ start.py
@@ -0,0 +1,9 @@
+from Game import start
+
+
+def main():
+    start()
+
+
+if __name__ == '__main__':
+    main()
