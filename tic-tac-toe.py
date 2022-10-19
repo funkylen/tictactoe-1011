2bae061a8434e5744c31453289efa6f077312323
houston13 <57140304+gulfiya33@users.noreply.github.com>
2022-10-19 19:56:02 +0300

    Update tic-tac-toe.py

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -18,32 +18,33 @@ buttons = [
     [None, None, None]
 ]
 
-def victory_check():   
-    if state[0][0]==state[1][1]==state[2][2] != None or state[2][0]==state[1][1]==state[0][2] != None:
-        if state[1][1] == SIGN_X:
-            result = messagebox.showinfo("Game Over", "!!!X WIN!!!")
-        else:
-            result = messagebox.showinfo("Game Over", "!!!O WIN!!!")
-        sys.exit()    
-            
-    for i in range(3):
-        if state[i][0]==state[i][1]==state[i][2] != None or state[0][i]==state[1][i]==state[2][i] != None:
-            if state[i][i] == SIGN_X:
-                result = messagebox.showinfo("Game Over", "!!!X WIN!!!")
-            else:
-                result = messagebox.showinfo("Game Over", "!!!O WIN!!!")
-            sys.exit()
-                     
-    if noneCounter() == 0:
-        result = messagebox.showinfo("Game Over", "DRAWN GAME")
-        sys.exit()
-             
 
 def noneCounter():
     steps = 0
     for i in range(3):
         steps += state[i].count(None)
     return steps
+
+
+def victory_check(current_sign):   
+    if state[0][0]==state[1][1]==state[2][2] != None or state[2][0]==state[1][1]==state[0][2] != None:
+        title = Label(gui, text= "Game Over", font= ("Arial", 9, "bold")).grid(row= 3, column= 1)
+        gui.update()
+        result = messagebox.showinfo("Game Over", "!!!X WIN!!!") if current_sign == SIGN_X else messagebox.showinfo("Game Over", "!!!O WIN!!!")
+        sys.exit()    
+            
+    for i in range(3):
+        if state[i][0]==state[i][1]==state[i][2] != None or state[0][i]==state[1][i]==state[2][i] != None:
+            title = Label(gui, text= "Game Over", font= ("Arial", 9, "bold")).grid(row= 3, column= 1)
+            gui.update()
+            result = messagebox.showinfo("Game Over", "!!!X WIN!!!") if current_sign == SIGN_X else messagebox.showinfo("Game Over", "!!!O WIN!!!")
+            sys.exit()
+                     
+    if noneCounter() == 0:
+        title = Label(gui, text= "Game Over", font= ("Arial", 9, "bold")).grid(row= 3, column= 1)
+        gui.update()
+        result = messagebox.showinfo("Game Over", "DRAWN GAME")
+        sys.exit()
     
 
 def click(i,j):
@@ -58,7 +59,7 @@ def click(i,j):
         title = Label(gui, text= f'Ходит {title_sign}', font= ("Arial", 11, "bold")).grid(row= 3, column= 1)
         gui.update()
     
-    victory_check()
+    victory_check(current_sign)
     
 
 gui = Tk(className="tic-tac-toe")
@@ -66,9 +67,8 @@ gui.geometry("230x290")
 
 for i in range(3):
     for j in range(3):                   
-        buttons[i][j] = Button(height = 2, width = 4, bg = '#A18594',font = ("Arial", 20, "bold"), command = lambda x = i, y = j : click(x,y))
+        buttons[i][j] = Button(height = 2, width = 4, bg = '#9370D8',font = ("Arial", 20, "bold"), command = lambda x = i, y = j : click(x,y))
         buttons[i][j].grid(row = i, column = j)
 
 title = Label(gui, text= "Start", font= ("Arial", 14, "bold")).grid(row= 3, column= 1)
-
 gui.mainloop()
