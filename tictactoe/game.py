0f06cc83c8e40a64e7bfd02b678b61b4938d6cb6
HiBlakk <hiblakk@gmail.com>
2022-10-20 21:08:23 +0300

    Рабочая версия

--- tictactoe/game.py
+++ tictactoe/game.py
@@ -1,25 +1,80 @@
 import prompt
 
+
 SIGN_X = 'X'
 SIGN_O = 'O'
+
 
 state = [
     [None, None, None],
     [None, None, None],
     [None, None, None]
-]
+        ]
+
 
 def check_index(index):
     min_value = 0
     max_value = 2
     return min_value <= index <= max_value
 
+
+def print_win_state(state):
+
+    print('Появился победитель') 
+
+    for row in range(len(state)): 
+        for col in range(len(state)): 
+
+            if state[row][col] == None: 
+                state[row][col] = '_'
+
+            print(state[row][col], end = ' ')
+
+        print()
+
+
+def check_win(state):
+   
+    min_index = 0 
+    max_index = 2
+
+    for row in range(len(state)):  
+        for col in range(len(state)):
+            
+            if row == min_index: 
+
+                if (state[row][col] == state [row+1][col] == state[row+2][col]) and state[row][col] is not None:
+                    return(f'Выиграл {str(state[row][col])} по {col+1}-oму столбцу') 
+
+                if col == min_index:
+
+                    if (state[row][col] == state [row][col+1] == state[row][col+2]) and state[row][col] is not None:
+                        return(f'Выиграл {str(state[row][col])} по {row+1}-ой строке')
+ 
+                if  row == col:
+
+                    if (state[row][col] == state [row+1][col+1] == state[row+2][col+2]) and state[row][col] is not None:
+                        return(f'Выиграл {str(state[row][col])} по главной диагонали')
+                    
+                if row + col == max_index:
+
+                    if (state[row][col] == state [row+1][col-1] == state[row+2][col-2]) and state[row][col] is not None:
+                        return(f'Выиграл {str(state[row][col])} по побочной диагонали') 
+            
+            elif col == min_index:
+
+                if (state[row][col] == state [row][col+1] == state[row][col+2]) and state[row][col] is not None:
+                    return(f'Выиграл {str(state[row][col])} по {row+1}-ой строке')
+
+    return False
+
+
 def start():
     steps = 9
     while steps != 0:
         current_sign = SIGN_O if steps % 2 == 0 else SIGN_X
 
-        print(f'Ходит {current_sign}')
+        print(f'Ход: {current_sign}')
 
         row_index = prompt.integer('Введите строку от 1 до 3: ') - 1
         col_index = prompt.integer('Введите столбец от 1 до 3: ') - 1
@@ -27,68 +82,27 @@ def start():
         if check_index(row_index) and check_index(col_index):
 
             if state[row_index][col_index] is not None:
-                print('Выберите другое место. Занято.')
+                print('Выберите другое место, так как это занято')
                 continue
 
             state[row_index][col_index] = current_sign
+            check_win(state)
 
-            for line in state:
-                print(line)
+            if check_win(state) != False:
+
+                print_win_state(state)
+                return print(check_win(state))
+
+            else: 
+
+                for line in state:
+                    print(line)
 
             steps -= 1
         else:
-            print('Неправильное значение столбца или строки') 
+            print('Неверное значение столбца или строки')
 
-def current_state(state): 
-    for i in range(len(state)): 
-        for j in range(len(state)):
-            if state[i][j] == None: 
-                state[i][j] = '_'
-            print(state[i][j], end = ' ') 
-        print()
+    print('Победа дружбы')
 
-def check_win(state):
-    current_state(state)
-    min_index = 0 
-    max_index = 2
-    for i in range(len(state)):  
-        for j in range(len(state)):
-            
-            if i == min_index: 
-                
-                if (state[i][j] == state [i+1][j] == state[i+2][j]) and state[i][j] != '_':
-                    return (f'Выиграл {str(state[i][j])} по {j+1}-oму столбцу')
-                if j == min_index:
-                    
-                    if (state[i][j] == state [i][j+1] == state[i][j+2]) and state[i][j] != '_':
-                        return (f'Выиграл {str(state[i][j])} по {i+1}-ой строке')
-def check_win(state):
-    current_state(state)
-    min_index = 0 
-    max_index = 2
-    for i in range(len(state)):  
-        for j in range(len(state)):
-            
-            if i == min_index: 
-                
-                if (state[i][j] == state [i+1][j] == state[i+2][j]) and state[i][j] != '_':
-                    return (f'Выиграл {str(state[i][j])} по {j+1}-oму столбцу')
-                if j == min_index:
-                    
-                    if (state[i][j] == state [i][j+1] == state[i][j+2]) and state[i][j] != '_':
-                        return (f'Выиграл {str(state[i][j])} по {i+1}-ой строке')
-                if  i == j:
-                    
-                    if (state[i][j] == state [i+1][j+1] == state[i+2][j+2]) and state[i][j] != '_':
-                        return (f'Выиграл {str(state[i][j])} по главной диагонали')
-                if i + j == max_index: 
-                    
-                    if (state[i][j] == state [i+1][j-1] == state[i+2][j-2]) and state[i][j] != '_':
-                        return (f'Выиграл {str(state[i][j])} по побочной диагонали')
-            elif j == min_index:
-                
-                if (state[i][j] == state [i][j+1] == state[i][j+2]) and state[i][j] != '_':
-                    return (f'Выиграл {str(state[i][j])} по {i+1}-ой строке')
-                
-    return 'Ничья'
+
 start()
