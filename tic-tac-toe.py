e860d73bd4f4a326c3796f5e9c6f448ffd489dee
houston13 <57140304+gulfiya33@users.noreply.github.com>
2022-10-22 11:28:53 +0300

    Update tic-tac-toe.py

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,6 +1,5 @@
 from tkinter import *
 from tkinter import messagebox
-import sys
 
 
 SIGN_X = 'X'
@@ -34,6 +33,12 @@ def clean_buttons():
             state[i][j] = None
 
 
+def insert(i,j,current_sign):
+    buttons[i][j].configure(text = current_sign)
+    buttons[i][j]["state"] = DISABLED
+    state[i][j] = current_sign
+
+
 def victory_check():   
     if state[0][0]==state[1][1]==state[2][2] != None or state[2][0]==state[1][1]==state[0][2] != None:
         return True    
@@ -47,20 +52,20 @@ def click(i,j):
     current_sign = SIGN_O if noneCounter() % 2 == 0 else SIGN_X
     title_sign = SIGN_O if current_sign == SIGN_X else SIGN_X    
 
-    buttons[i][j].configure(text = current_sign)
-    buttons[i][j]["state"] = DISABLED
-    state[i][j] = current_sign
+    insert(i,j,current_sign)
     
     if noneCounter() > 0:
         title = Label(gui, text= f'Ходит {title_sign}', font= ("Arial", 11, "bold")).grid(row= 3, column= 1)
     else:
         title = Label(gui, text= "Game Over", font= ("Arial", 9, "bold")).grid(row= 3, column= 1)
         result = messagebox.showinfo("Game Over", "DRAWN GAME")
+        title = Label(gui, text= "Start", width = 5, font= ("Arial", 14, "bold")).grid(row= 3, column= 1)
         clean_buttons()
 
     if victory_check() == True:
         title = Label(gui, text= "Game Over", font= ("Arial", 9, "bold")).grid(row= 3, column= 1)
         result = messagebox.showinfo("Game Over", f"!!!{current_sign} WIN!!!") 
+        title = Label(gui, text= "Start", width = 5, font= ("Arial", 14, "bold")).grid(row= 3, column= 1)
         clean_buttons()
     gui.update()
     
